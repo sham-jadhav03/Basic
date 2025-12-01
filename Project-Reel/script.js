9c06576275e4649a7147b8aacd18658099c1f231
sham jadhav <ghanshamjadhav2003@gmail.com>
2025-12-01 19:34:33 +0530

    reel project

--- Project-Reel/script.js
+++ Project-Reel/script.js
@@ -8,7 +8,7 @@ const posts = [
     isFollowed: true,
     caption: "Building my JS empire one bug at a time 💻🔥",
     video: "video/video.mp4",
-    userProfile: "profiles/sham.png"
+    userProfile: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg"
   },
   {
     username: "code_master",
@@ -18,8 +18,8 @@ const posts = [
     shareCount: 20,
     isFollowed: false,
     caption: "React + Tailwind = clean UI supremacy ✨",
-    video: "videos/video1.mp4",
-    userProfile: "profiles/master.png"
+    video: "video/video1.mp4",
+    userProfile: "https://images.pexels.com/photos/2641022/pexels-photo-2641022.jpeg"
   },
   {
     username: "frontend_freak",
@@ -29,19 +29,19 @@ const posts = [
     shareCount: 7,
     isFollowed: true,
     caption: "CSS animations are basically sorcery 😭💫",
-    video: "videos/video3.mp4",
-    userProfile: "profiles/freak.png"
+    video: "video/video3.mp4",
+    userProfile: "https://images.pexels.com/photos/27765641/pexels-photo-27765641.jpeg"
   },
    {
-    username: "code_master",
+    username: "Beauty_master",
     likeCount: 942,
     isLiked: true,
     commentCount: 88,
     shareCount: 20,
     isFollowed: false,
-    caption: "React + Tailwind = clean UI supremacy ✨",
-    video: "videos/video4.mp4",
-    userProfile: "profiles/master.png"
+    caption: " clean and ashthetic #explore ✨",
+    video: "video/video4.mp4",
+    userProfile: "https://images.pexels.com/photos/29498623/pexels-photo-29498623.jpeg"
   },
    {
     username: "riya_18",
@@ -51,41 +51,39 @@ const posts = [
     shareCount: 20,
     isFollowed: false,
     caption: "React + Tailwind = clean UI supremacy ✨",
-    video: "videos/video2.mp4",
+    video: "video/video2.mp4",
     userProfile: "https://images.unsplash.com/photo-1591118490115-9f51ee41094c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHx8"
   },
 ];
 
 var sum = ''
-posts.forEach((elem)=>{
-  sum = sum + ` <div class="reel">
-          <video class="main-img"
-            src="${elem.video}"
-            alt="">
-        </div>
-        <div class="bottom">
-          <div class="user">
-            <img src="${elem.userProfile}" autoplay muted alt="">
-            <h4>${elem.username}</h4>
-            <button>${elem.isFollowed}</button>
+posts.forEach((elem, idx)=>{
+  sum = sum + `<div class="reel">
+          <video autoplay loop muted src="${elem.video}"></video>
+          <div class="bottom">
+            <div class="user">
+              <img src="${elem.userProfile}" alt="">
+              <h4>${elem.username}</h4>
+              <button>${elem.isFollowed?'Unfollow':'Follow'}</button>
+            </div>
+            <h3>${elem.caption}</h3>
           </div>
-          <h3>${elem.caption}</h3>
-        </div>
-        <div class="right">
-          <div class="like">
-            <h4 class="like-icon icon">liked<i class="love ri-heart-3-fill"></i></i>'</h4>
-            <h6>${elem.likeCount}</h6>
-          </div>
-          <div class="comment">
-            <h4 class="comment-icon icon"><i class="ri-chat-3-line"></i></h4>
-            <h6>${elem.commentCount}</h6>
-          </div>
-          <div class="share">
-            <h4 class="share-icon icon"><i class="ri-share-forward-line"></i></h4>
-            <h6>${elem.shareCount}</h6>
-          </div>
-          <div class="menu">
-            <h4 class="menu-icon icon"><i class="ri-more-2-fill"></i></h4>
+          <div class="right">
+            <div id=${idx} class="like">
+              <h4 class="like-icon icon">${elem.isLiked ? '<i class="love ri-heart-3-fill"></i>' : '<i class="ri-heart-3-line"></i>'}</h4>
+              <h6>${elem.likeCount}</h6>
+            </div>
+            <div class="comment">
+              <h4 class="comment-icon icon"><i class="ri-chat-3-line"></i></h4>
+              <h6>${elem.commentCount}</h6>
+            </div>
+            <div class="share">
+              <h4 class="share-icon icon"><i class="ri-share-forward-line"></i></h4>
+              <h6>${elem.shareCount}</h6>
+            </div>
+            <div class="menu">
+              <h4 class="menu-icon icon"><i class="ri-more-2-fill"></i></h4>
+            </div>
           </div>
         </div>`
 })
